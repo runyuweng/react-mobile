b3a25f958f749e98ca64e0f9eeabb497245605b9
wengwengweng <593110501@qq.com>
2017-02-26 17:21:03 +0800

    update

--- dazhao-mobile/src/components/Index/Index.jsx
+++ dazhao-mobile/src/components/Index/Index.jsx
@@ -10,7 +10,7 @@ class Index extends React.Component {
         <header>
           <img src="/src/images/banner1.jpg"/>
           <div className="search">
-            <input type="text"/>
+            <input type="text" placeholder="课程名称"/>
             <button>
               <img src="/src/images/searchico.png"/>
             </button>
@@ -26,6 +26,7 @@ class Index extends React.Component {
             <div className="title"><span>课程预告</span></div>
             <div className="content-large">
               <img src="/src/images/1487917069l108992947.png"/>
+              <div className="tips">2月25日  15：00-19：00</div>
             </div>
           </section>
           <section>
@@ -38,6 +39,45 @@ class Index extends React.Component {
               <div className="content">
                 <img src="/src/images/1481979697l859459990.png"/>
                 <div className="info">读MBTI，如何更好...</div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="title"><span>职场通识</span></div>
+            <div className="content-two">
+              <div className="content">
+                <img src="/src/images/1481979697l859459990.png"/>
+                <div className="info">读MBTI，如何更好...</div>
+              </div>
+              <div className="content">
+                <img src="/src/images/1481373795l515443385.png"/>
+                <div className="info">情商低到底有多可...</div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="title"><span>行业解读</span></div>
+            <div className="content-two">
+              <div className="content">
+                <img src="/src/images/1487917069l108992947.png"/>
+                <div className="info">基于大数据的人工...</div>
+              </div>
+              <div className="content">
+                <img src="/src/images/1481189424l698185646.png"/>
+                <div className="info">资本运作弄潮儿--...</div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="title"><span>学长学姐</span></div>
+            <div className="content-two">
+              <div className="content">
+                <img src="/src/images/1478935977l996178167.jpg"/>
+                <div className="info">如何跨专业准备...</div>
+              </div>
+              <div className="content">
+                <img src="/src/images/1478937533l353542196.jpg"/>
+                <div className="info">应届生应该选择大...</div>
               </div>
             </div>
           </section>
