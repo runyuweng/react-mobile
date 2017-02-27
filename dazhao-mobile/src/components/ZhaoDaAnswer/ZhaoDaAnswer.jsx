a9a75344963538a34c065cc28f8ebf9d7bcf0682
wangxianfa <1270386187@qq.com>
2017-02-27 21:48:49 +0800

    问题修改

---
+++ dazhao-mobile/src/components/ZhaoDaAnswer/ZhaoDaAnswer.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import "./ZhaoDaAnswer.scss";
+import { Link } from 'react-router';
+import QueueAnim from 'rc-queue-anim';
+
+class ZhaoDaAnswer extends React.Component {
+  render() {
+    return(
+      <div className="ZhaoDaAnswer">
+          <article>
+            <span className="topic">话题：<i>考研</i></span>
+            <p className="theme">研究生和本科学历在求职过程中真的会有很大差别吗？</p>
+            <div className="publisher">
+              Michal
+              <span className="vip"><img src="/src/images/vip.png"/></span>，
+              <span>骨灰级教练</span>
+            </div>
+            <div className="comment">
+              这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...
+            </div>
+            <div className="more">
+              <span><b><img src="/src/images/zan.png"/></b>赞同9</span>
+              <span><b><img src="/src/images/comment.png"/></b>评论14</span>
+              <span><b><img src="/src/images/cang.png"/></b>收藏</span>
+            </div>
+          </article>
+
+          <article>
+            <span className="topic">话题：<i>考研</i></span>
+            <p className="theme"><strong>研究生和本科学历在求职过程中真的会有很大差别吗？</strong></p>
+            <div className="publisher">
+              Michal
+              <span className="vip"><img src="/src/images/vip.png"/></span>，
+              <span>骨灰级教练</span>
+            </div>
+            <div className="comment">
+              这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...
+            </div>
+            <div className="more">
+              <span><b><img src="/src/images/zan.png"/></b>赞同9</span>
+              <span><b><img src="/src/images/comment.png"/></b>评论14</span>
+              <span><b><img src="/src/images/cang.png"/></b>收藏</span>
+            </div>
+          </article>
+
+          <article>
+            <span className="topic">话题：<i>考研</i></span>
+            <p className="theme"><strong>研究生和本科学历在求职过程中真的会有很大差别吗？</strong></p>
+            <div className="publisher">
+              Michal
+              <span className="vip"><img src="/src/images/vip.png"/></span>，
+              <span>骨灰级教练</span>
+            </div>
+            <div className="comment">
+              这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...
+            </div>
+            <div className="more">
+              <span><b><img src="/src/images/zan.png"/></b>赞同9</span>
+              <span><b><img src="/src/images/comment.png"/></b>评论14</span>
+              <span><b><img src="/src/images/cang.png"/></b>收藏</span>
+            </div>
+          </article>
+
+          <article>
+            <span className="topic">话题：<i>考研</i></span>
+            <p className="theme"><strong>研究生和本科学历在求职过程中真的会有很大差别吗？</strong></p>
+            <div className="publisher">
+              Michal
+              <span className="vip"><img src="/src/images/vip.png"/></span>，
+              <span>骨灰级教练</span>
+            </div>
+            <div className="comment">
+              这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...
+            </div>
+            <div className="more">
+              <span><b><img src="/src/images/zan.png"/></b>赞同9</span>
+              <span><b><img src="/src/images/comment.png"/></b>评论14</span>
+              <span><b><img src="/src/images/cang.png"/></b>收藏</span>
+            </div>
+          </article>
+
+          <article>
+            <span className="topic">话题：<i>考研</i></span>
+            <p className="theme"><strong>研究生和本科学历在求职过程中真的会有很大差别吗？</strong></p>
+            <div className="publisher">
+              Michal
+              <span className="vip"><img src="/src/images/vip.png"/></span>，
+              <span>骨灰级教练</span>
+            </div>
+            <div className="comment">
+            <span><img src="/src/images/music.png"/><span className="start"><img src="/src/images/start.png"/></span><b>1'57''</b></span>
+            </div>
+            <div className="more">
+              <span><b><img src="/src/images/zan.png"/></b>赞同9</span>
+              <span><b><img src="/src/images/comment.png"/></b>评论14</span>
+              <span><b><img src="/src/images/cang.png"/></b>收藏</span>
+            </div>
+          </article>
+
+      </div>
+    )
+  }
+}
+export default ZhaoDaAnswer;
