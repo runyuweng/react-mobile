3ada65117989777e04f5234b510488c9dc905863
wangxianfa <1270386187@qq.com>
2017-02-27 11:37:37 +0800

    发现和专栏

---
+++ dazhao-mobile/src/components/ZhaoDaFeature/ZhaoDaFeature.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./ZhaoDaFeature.scss";
+import { Link } from 'react-router';
+import QueueAnim from 'rc-queue-anim';
+
+class ZhaoDaFeature extends React.Component {
+  render() {
+    return(
+      <div className="ZhaoDaFeature">
+        <div className="feature">
+          <div className="featureImg"><img src="/src/images/1481373795l515443385.png"/>
+          </div>
+          <span className="fTheme">#麦力达#第一期--考研那些事儿..</span>
+          <div className="publisher">
+            <span className="cicle"></span>
+            Michal
+            <span className="vip"><img src="/src/images/vip.png"/></span>
+          </div>
+          <p>读研？工作？跨专业？选热门？如何选择变成现在最合适的 如何选择编程未来最正确的？</p>
+        </div>
+
+        <div className="feature">
+          <div className="featureImg"><img src="/src/images/1481373795l515443385.png"/>
+          </div>
+          <span className="fTheme">#麦力达#第一期--考研那些事儿..</span>
+          <div className="publisher">
+            <span className="cicle"></span>
+            Michal
+            <span className="vip"><img src="/src/images/vip.png"/></span>
+          </div>
+          <p>读研？工作？跨专业？选热门？如何选择变成现在最合适的 如何选择编程未来最正确的？</p>
+        </div>
+
+        <div className="feature">
+          <div className="featureImg"><img src="/src/images/1481373795l515443385.png"/>
+          </div>
+          <span className="fTheme">#麦力达#第一期--考研那些事儿..</span>
+          <div className="publisher">
+            <span className="cicle"></span>
+            Michal
+            <span className="vip"><img src="/src/images/vip.png"/></span>
+          </div>
+          <p>读研？工作？跨专业？选热门？如何选择变成现在最合适的 如何选择编程未来最正确的？</p>
+        </div>
+
+
+      </div>
+    )
+  }
+}
+export default ZhaoDaFeature;
