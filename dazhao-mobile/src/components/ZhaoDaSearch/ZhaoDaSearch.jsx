1f094b7f5a4385261069dadb21ef8bf7cf7a9e05
wangxianfa <1270386187@qq.com>
2017-03-01 21:23:46 +0800

    search页面整理

--- dazhao-mobile/src/components/ZhaoDaSearch/ZhaoDaSearch.jsx
+++ dazhao-mobile/src/components/ZhaoDaSearch/ZhaoDaSearch.jsx
@@ -4,14 +4,55 @@ import { Link } from 'react-router';
 import QueueAnim from 'rc-queue-anim';
 
 class ZhaoDaSearch extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      Keyword:'',
+      response:[
+        {
+          id:1,
+          title:'研究生和本科学历在求职过程中真的会有很大差别吗？',
+          answer:'这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...'
+        },
+        {
+          id:2,
+          title:'研究生和本科学历在求职过程中真的会有很大差别吗？',
+          answer:'这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...'
+        },
+        {
+          id:3,
+          title:'研究生和本科学历在求职过程中真的会有很大差别吗？',
+          answer:'这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚研究生学...'
+        }
+      ]
+    }
+  }
 
   render() {
+
+    const {Keyword,response}=this.state;
+    const responseList = response.map(function(item) {
+      return (
+          <div className="items" key={item.id}>
+            <Link to = "/response"><span>{item.title}</span></Link>
+            <p>{item.answer}</p>
+          </div>
+        );
+    })
+    console.log(responseList);
     return(
       <div className="ZhaoDaSearch">
         <header>
           <div className="search">
               <span >取消</span>
-              <input type="text" placeholder="研究生"/>
+              <input type="text"
+              placeholder="研究生"
+              value={Keyword}
+              onChange={(e)=>{
+                this.setState({
+                  Keyword:e.target.value
+                });
+              }}/>
               <span>搜索</span>
           </div>
         </header>
@@ -24,9 +65,10 @@ class ZhaoDaSearch extends React.Component {
           </ul>
         </nav>
         <div className="getting">
+        {/*
           <div className="items">
             <Link to = "/response"><span><b>研究生</b>和本科学历在求职过程中真的会有很大差别吗？</span></Link>
-            <p>这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚<b>研究生学</b>...</p>
+            <p>这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚<b>研究生</b>学...</p>
           </div>
 
           <div className="items">
@@ -43,7 +85,8 @@ class ZhaoDaSearch extends React.Component {
             <span><b>研究生</b>和本科学历在求职过程中真的会有很大差别吗？</span>
             <p>这个问题，还得要看企业的需求，比如说一些企业的技术岗位，这些企业在招聘介绍里就会写清楚<b>研究生学</b>...</p>
           </div>
-
+            */}
+           {responseList}
         </div>
       </div>
     )
