606c3c93b8260d260ae0efe0ceb0e44e56a0802e
wengwengweng <593110501@qq.com>
2017-05-10 22:00:53 +0800

    update

--- dazhao-mobile/src/components/Zhaoda/ZhaoDaTopic/ZhaoDaTopic.jsx
+++ dazhao-mobile/src/components/Zhaoda/ZhaoDaTopic/ZhaoDaTopic.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./ZhaoDaTopic.scss";
 import TopBar from "../../MainLayout/TopBar/TopBar.jsx";
 import {Link} from "react-router";
+import ajax from "../../../services/ajax.js";
 
 class ZhaoDaTopic extends React.Component {
 
@@ -13,81 +14,8 @@ class ZhaoDaTopic extends React.Component {
             "nowX": 0, // 鼠标移动后的位置
             "delateX": 0, // 移动的距离
             "currentX": 0,
-            "topicCategory": [
-                {
-                    "id": 0,
-                    "topicName": "全部"
-                },
-                {
-                    "id": 1,
-                    "topicName": "行业"
-                },
-                {
-                    "id": 2,
-                    "topicName": "专栏"
-                }, {
-                    "id": 3,
-                    "topicName": "求职"
-                },
-                {
-                    "id": 4,
-                    "topicName": "技能"
-                },
-                {
-                    "id": 5,
-                    "topicName": "名企"
-                },
-                {
-                    "id": 6,
-                    "topicName": "求职"
-                },
-                {
-                    "id": 7,
-                    "topicName": "技能"
-                },
-                {
-                    "id": 8,
-                    "topicName": "名企"
-                }
-            ],
-            "topics": [
-                {
-                    "id": 0,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                },
-                {
-                    "id": 1,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                },
-                {
-                    "id": 2,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                },
-                {
-                    "id": 3,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                },
-                {
-                    "id": 4,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                },
-                {
-                    "id": 5,
-                    "topicImg": "/src/images/topicImg.png",
-                    "topicTitle": "职业素养",
-                    "questionNum": 24
-                }
-            ],
+            "topicCategory": [],
+            "topics": [],
             "active": 0
         };
         this.topicClick = this.topicClick.bind(this);
@@ -102,11 +30,27 @@ class ZhaoDaTopic extends React.Component {
 
         this._touchEvent(this.refs.navbar);
 
+        ajax({url:'/zhaoda/topic/topiccategory'})
+        .then((data)=>{
+          console.log(data);
+
+          this.setState({topicCategory:data.contents})
+        })
+
+        ajax({"url": "/zhaoda/topic/hottopics?categoryid=-1 "}).
+        then((data) => {
+              this.setState({"topics": data.contents});
+        });
+
     }
 
-    topicClick (index) {
+    topicClick (index ,id) {
 
         this.setState({"active": index});
+        ajax({"url": `/zhaoda/topic/hottopics?categoryid=${id}`}).
+        then((data) => {
+              this.setState({"topics": data.contents});
+        });
 
     }
 
@@ -199,13 +143,14 @@ class ZhaoDaTopic extends React.Component {
     render () {
 
         const {topicCategory, topics, active} = this.state;
-        const topicCategoryList = topicCategory.map((elem, index) => <li onClick={this.topicClick.bind(this, index)} className={active == index ? "active" : ""} key={elem.id}>{elem.topicName}</li>);
+        console.log(topics);
+        const topicCategoryList = topicCategory.map((elem, index) => <li onClick={this.topicClick.bind(this, index ,elem.id)} className={active == index ? "active" : ""} key={index}>{elem.topictypename}</li>);
         const topicsList = topics.map((elem, index) =>
-            <Link to="/totopic" key={index}>
+            <Link to={`/totopic/${elem.tid}`} key={index}>
                 <div className="item" key={index}>
-                    <span><img src={elem.topicImg} /></span>
-                    <p>{elem.topicTitle}</p>
-                    <p>{elem.questionNum}个问题</p>
+                    <span><img src="/src/images/topicImg.png" /></span>
+                    <p>{elem.topicname}</p>
+                    <p>{elem.questionnum}个问题</p>
                 </div>
             </Link>
             );
